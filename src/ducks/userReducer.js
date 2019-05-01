c2373696fecfc5694d5d66dd7ba8942a6aa9870f
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-05-01 16:24:56 -0600

    Step 4

---
+++ src/ducks/userReducer.js
@@ -0,0 +1,16 @@
+
+const initialState = {
+  email: null,
+  firstName: null,
+  lastName: null
+}
+
+
+
+export default function userReducer(state = initialState, action) {
+  switch (action.type) {
+
+    default: 
+      return state
+  }
+}
